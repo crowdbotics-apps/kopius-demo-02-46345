2d14569ca9ac8a25e0ccb7e0f11ede09974a6925
Crowdbotics <team@crowdbotics.com>
2024-02-14 17:05:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kopius-demo-02-46345",
+  url: "https://kopius-demo-02-46345.botics.co",
+  api: "https://kopius-demo-02-46345.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
